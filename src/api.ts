5d49071efb120f3d14237acf06201f9200e7cb55
ti-dolphin <ti.dse01@gmail.com>
2024-07-12 08:13:45 -0300

    using google cloud server

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //PROD: https://requisicoes-backend.vercel.app
-//http://localhost:3002
+//http://localhost:3000
 //currentProd : http://35.198.25.52
 const api = axios.create({
   baseURL: "http://35.198.25.52", // Substitua pela URL do seu backend
