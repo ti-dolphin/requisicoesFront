b9c0880421cfb1b24100553ac2b372aac047153a
ti-dolphin <ti.dse01@gmail.com>
2024-07-26 08:57:52 -0300

    layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -12,13 +12,13 @@ import { useNavigate } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { userContext } from "../../context/userContext";
 const modules = [
-  { 
-    image: 'https://firebasestorage.googleapis.com/v0/b/dolphin-8f800.appspot.com/o/homeBg-worker.jpeg?alt=media&token=d94f7107-0108-40c7-98e7-7076f017f96b',
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/dolphin-8f800.appspot.com/o/homeBg-worker.jpeg?alt=media&token=d94f7107-0108-40c7-98e7-7076f017f96b",
     name: "Dolphin Requisições",
     Info: "Faça aqui suas Requisições de Materiais/Serviços",
-    path: '/requisitions'
+    path: "/requisitions",
   },
-
 ];
 const Home = () => {
     const navigate = useNavigate();
@@ -34,7 +34,6 @@ const Home = () => {
   return (
     <Box
       sx={{
-        border: "1px solid #e3e3e3",
         backgroundRepeat: "no-repeat",
         backgroundSize: "contain",
         backgroundPosition: "center",
@@ -49,40 +48,49 @@ const Home = () => {
           backgroundPosition: "left",
           backgroundSize: "contain",
           backgroundRepeat: "no-repeat",
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
           width: "100%",
           height: "10%",
         }}
+      ></Box>
+
+      <Box
+        sx={{
+          height: '100%',
+          overflowY: "auto",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "start",
+          gap: '2rem',
+          alignItems: "center",
+        }}
       >
+        {" "}
         <Typography
           sx={{
             textAlign: "center",
             fontWeight: "600",
             color: "#1e549f",
             fontSize: "2rem",
+
           }}
         >
           Bem Vindo ao Dolphin Controle
         </Typography>
-      </Box>
-
-      <Box
-        sx={{
-          height: "60%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Stack direction="row" spacing={4}>
+        <Stack width="100%" justifyContent="center" flexWrap="wrap"  direction="row" spacing={1}>
           {modules.map((module) => (
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea onClick={() => handleNavigateToModule(module.path)}>
+            <Card sx={{ maxWidth: 345,  }}>
+              <CardActionArea
+                onClick={() => handleNavigateToModule(module.path)}
+              >
                 <CardMedia
+                  sx={{
+                    minWidth: '315px',
+                    maxHeight: "300px",
+                  }}
                   component="img"
-                  height="140"
                   image={module.image}
                   alt="green iguana"
                 />
