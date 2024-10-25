9500589e67387bbc11426eb8da743116ab023ad3
ti-dolphin <ti.dse01@gmail.com>
2024-10-25 15:08:07 -0300

    accessory improvments

--- src/Patrimony/modals/PatrimonyAccessoriesModal.tsx
+++ src/Patrimony/modals/PatrimonyAccessoriesModal.tsx
@@ -8,33 +8,32 @@ import {
   IconButton,
   Stack,
   Badge,
-  ListItem,
-  ListItemButton,
-  ListItemText,
   useMediaQuery,
   useTheme,
   Theme,
   Button,
-  BadgeProps,
   styled,
+  Card,
+  CardContent,
+  CardMedia,
+  CircularProgress,
 } from "@mui/material";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
 import CloseIcon from "@mui/icons-material/Close";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { PatrimonyInfoContext } from "../context/patrimonyInfoContext";
 import {
+  createPatrimonyAccessoryFile,
   deletePatrimonyAccessory,
   getAccessoriesByPatrimonyId,
   getPatrimonyAccessoryFiles,
 } from "../utils"; // Assuming this function exists
 import { PatrimonyAccessory, PatrimonyAccessoryFile } from "../types";
-import { FixedSizeList, ListChildComponentProps } from "react-window";
-import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
 import { useParams } from "react-router-dom";
-import PatrimonyAccessoryFileModal from "./PatrimonyAccessoryFileModal";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import CreatePatrimonyAccessoryModal from "./CreatePatrimonyAccessory";
-
+import CameraFileLogo from '../assets/cameraFileLogo-Chy76Qi9-Chy76Qi9.png';
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
 // Styles using breakpoints for responsiveness
 const modalStyle = (theme: Theme) => ({
   position: "absolute" as const,
@@ -43,8 +42,13 @@ const modalStyle = (theme: Theme) => ({
   transform: "translate(-50%, -50%)",
   width: "90%",
   maxWidth: 600,
+  maxHeight: 600,
   bgcolor: "background.paper",
   boxShadow: 24,
+  display: 'flex',
+  flexDirection: "column",
+  overflowY: 'scroll',
+  gap: '2rem',
   p: 4,
   [theme.breakpoints.down("sm")]: {
     width: "100%", // Full width on mobile
@@ -63,29 +67,7 @@ const style = {
   p: 4,
 };
 
-const innerModalStyle = (theme: Theme) => ({
-  position: "absolute" as const,
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "80%",
-  maxWidth: 400,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4,
-  [theme.breakpoints.down("sm")]: {
-    width: "90%",
-  },
-});
 
-const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
-  "& .MuiBadge-badge": {
-    right: -3,
-    top: 13,
-    border: `2px solid ${theme.palette.background.paper}`,
-    padding: "0 4px",
-  },
-}));
 
 export default function PatrimonyAccessoryModal() {
   const { id_patrimonio } = useParams();
@@ -98,11 +80,11 @@ export default function PatrimonyAccessoryModal() {
     toggleRefreshPatrimonyAccessory,
   } = useContext(PatrimonyInfoContext);
   const [accessories, setAccessories] = useState<PatrimonyAccessory[]>([]);
-  const [selectedAccessory, setSelectedAccessory] =
     useState<PatrimonyAccessory | null>(null);
   const [open, setOpen] = useState<boolean>(false);
   const [accessoryFileMap, setAccessoryFileMap] =
-    useState<Map<number, PatrimonyAccessoryFile[]>>();
+    useState<Map<PatrimonyAccessory, PatrimonyAccessoryFile[]>>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
@@ -121,7 +103,7 @@ export default function PatrimonyAccessoryModal() {
         accessory.id_acessorio_patrimonio
       );
       if (files) {
-        tempMap.set(accessory.id_acessorio_patrimonio, files);
+        tempMap.set(accessory, files);
       }
     }
     setAccessoryFileMap(tempMap);
@@ -137,57 +119,8 @@ export default function PatrimonyAccessoryModal() {
 
   const handleClose = () => setOpen(false);
 
-  const handleOpenInnerModal = (accessory: PatrimonyAccessory) => {
-    setSelectedAccessory(accessory);
-  };
 
-  const handleCloseInnerModal = () => {
-    setSelectedAccessory(null);
-  };
 
-  function RenderRow(
-    props: ListChildComponentProps & { accessories: PatrimonyAccessory[] }
-  ) {
-    const { index, style, accessories } = props;
-    const { toggleDeletingPatrimonyAccessory } =
-      useContext(PatrimonyInfoContext);
-    const filesCounter = () => {
-      console.log(accessoryFileMap);
-      if (accessoryFileMap)
-        return accessoryFileMap.get(accessories[index].id_acessorio_patrimonio)
-          ?.length;
-    };
-    return (
-      <ListItem style={style} key={index} component="div">
-        <ListItemButton>
-          <Stack
-            direction="row"
-            alignItems="center"
-            width="100%"
-            justifyContent="space-between"
-          >
-            <ListItemText secondary={`${accessories[index].nome}`} />
-            <Stack direction="row">
-              <IconButton
-                onClick={() => handleOpenInnerModal(accessories[index])}
-              >
-                <StyledBadge badgeContent={filesCounter()} color="secondary">
-                  <AttachFileIcon sx={{ color: "#F7941E" }} />
-                </StyledBadge>
-              </IconButton>
-              <IconButton
-                onClick={() =>
-                  toggleDeletingPatrimonyAccessory(true, accessories[index])
-                }
-              >
-                <DeleteIcon sx={{ color: "#F7941E" }} />
-              </IconButton>
-            </Stack>
-          </Stack>
-        </ListItemButton>
-      </ListItem>
-    );
-  }
 
   const handleOpenCreatePatrimonyAccessory = () => {
     toggleCreatingPatrimonyAccessory();
@@ -210,6 +143,33 @@ export default function PatrimonyAccessoryModal() {
       }
     }
   };
+  const renderAcessoryImage = (accessory : PatrimonyAccessory ) => { 
+      if(accessoryFileMap?.get(accessory)){ 
+        const files = accessoryFileMap?.get(accessory);
+        if (files && files[0]) return files[files.length - 1].arquivo;
+        return CameraFileLogo;
+      }
+  };
+   const handleUploadFile = async (e: React.ChangeEvent<HTMLInputElement>, accessoryId: number) => {
+
+     if (e.target.files) {
+       setIsLoading(true);
+       const file = e.target.files[0];
+       const formData = new FormData();
+       formData.append("file", file);
+       const response = await createPatrimonyAccessoryFile(
+         Number(accessoryId),
+         formData
+       );
+       if (response && response.status === 200) {
+         setIsLoading(false);
+         toggleRefreshPatrimonyAccessory();
+         return;
+       }
+       window.alert("Erro ao fazer upload!");
+       setIsLoading(false);
+     }
+   };
   return (
     <div>
       <Badge badgeContent={accessories.length} color="primary">
@@ -235,30 +195,69 @@ export default function PatrimonyAccessoryModal() {
               </IconButton>
             </Stack>
           </Stack>
-          <FixedSizeList
-            height={isMobile ? 300 : 400}
-            width="100%"
-            itemSize={isMobile ? 70 : 46}
-            itemCount={accessories.length}
-            overscanCount={5}
-          >
-            {({ index, style }) => (
-              <RenderRow
-                index={index}
-                style={style}
-                accessories={accessories}
-                data={undefined}
-              />
-            )}
-          </FixedSizeList>
-          {selectedAccessory && (
-            <PatrimonyAccessoryFileModal
-              open={!!selectedAccessory}
-              selectedAccessory={selectedAccessory}
-              handleCloseInnerModal={handleCloseInnerModal}
-              innerModalStyle={innerModalStyle(theme)}
-            />
-          )}
+
+          <Stack direction="column" gap="1rem" alignItems="center">
+            {accessories &&
+              accessories.map((accessory, index) => (
+                <Card sx={{ borderRadius: "10px", width: 300 }}>
+                  <CardMedia
+                    image={isLoading ? '' : renderAcessoryImage(accessory)}
+                    sx={{ height: 200 }}
+                  >
+                    {isLoading && (
+                      <Stack
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                        sx={{ mt: 2, height: "100%" }}
+                      >
+                        <CircularProgress />
+                      </Stack>
+                    )}
+                  </CardMedia>
+                  <CardContent>
+                    <Stack
+                      direction="row"
+                      alignItems="center"
+                      justifyContent="space-between"
+                    >
+                      <Typography textTransform="capitalize" color="blue">
+                        {accessory.nome}
+                      </Typography>
+
+                      <IconButton
+                        onClick={() =>
+                          toggleDeletingPatrimonyAccessory(
+                            true,
+                            accessories[index]
+                          )
+                        }
+                      >
+                        <DeleteIcon sx={{ color: "#F7941E" }} />
+                      </IconButton>
+                    </Stack>
+                    <Button
+                      component="label"
+                      variant="contained"
+                      startIcon={<CloudUploadIcon />}
+                      sx={{
+                        mt: 2,
+                        width: isMobile ? "100%" : "auto", // Responsividade no botão
+                      }}
+                    >
+                      Anexar
+                      <VisuallyHiddenInput
+                        accept="image/*,application/pdf"
+                        onChange={(e) =>
+                          handleUploadFile(e, accessory.id_acessorio_patrimonio)
+                        }
+                        type="file"
+                      />
+                    </Button>
+                  </CardContent>
+                </Card>
+              ))}
+          </Stack>
           <CreatePatrimonyAccessoryModal />
           <Modal
             open={deletingPatrimonyAccessory[0]}
@@ -303,3 +302,14 @@ export default function PatrimonyAccessoryModal() {
     </div>
   );
 }
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
