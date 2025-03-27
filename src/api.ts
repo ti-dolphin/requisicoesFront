cb4b6782ae59f9b2930b349e662dca95c70aca44
ti-dolphin <64789914+ti-dolphin@users.noreply.github.com>
2025-03-27 13:04:03 -0300

    Create api.ts

---
+++ src/api.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+//PROD: https://apicontrole.dse.com.br
+//HOMOLOG: https://apicontrolehomologacao.dse.com.br
+//http://localhost:3001
+//http://localhost:3000
+
+export const api = axios.create({
+  baseURL: "https://apicontrolehomologacao.dse.com.br",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: window.localStorage.getItem("token"),
+  },
+});
+
+api.interceptors.request.use(
+  function (config) {
+    const token = window.localStorage.getItem("token") || "";
+    config.headers["Authorization"] = token;
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+}   
+);
+
+api.interceptors.response.use(
+  function(response){
+    return response;
+  },
+  function(error) {
+   console.log("error message: ", error.response.data.message);
+    if (
+      !window.localStorage.getItem("token") ||
+      error.response.data.message === "Not authorized"
+    ) {
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
+
+//ALTERAÇÔES
