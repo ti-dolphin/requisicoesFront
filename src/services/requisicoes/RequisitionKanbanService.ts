eac2b6862e6b32826eed95aa21689c768239fc66
Leonardo Pias da  Rocha <leorocha2004@gmail.com>
2026-09-03 10:10:01 -0300

    inicio att req

--- src/services/requisicoes/RequisitionKanbanService.ts
+++ src/services/requisicoes/RequisitionKanbanService.ts
@@ -1,40 +1,49 @@
 import api from "../../api";
 import { RequisitionKanban } from "../../models/requisicoes/RequisitionKanban";
 
-
 const ENDPOINT = "requisicao_kanban";
 
 const RequisitionKanbanService = {
-    async create(data: Partial<RequisitionKanban>): Promise<RequisitionKanban> {
-        const response = await api.post<RequisitionKanban>(`/${ENDPOINT}`, data);
-        return response.data;
-    },
+  async create(data: Partial<RequisitionKanban>): Promise<RequisitionKanban> {
+    const response = await api.post<RequisitionKanban>(`/${ENDPOINT}`, data);
+    return response.data;
+  },
 
-    async getMany(query?: any): Promise<RequisitionKanban[]> {
-        const response = await api.get<RequisitionKanban[]>(`/${ENDPOINT}`, {
-          params: query,
-        });
-        return response.data;
-    },
+  async getMany(query?: any): Promise<RequisitionKanban[]> {
+    const response = await api.get<RequisitionKanban[]>(`/${ENDPOINT}`, {
+      params: query,
+    });
+    return response.data;
+  },
 
-    async getById(id_kanban_requisicao: number): Promise<RequisitionKanban | null> {
-        try {
-            const response = await api.get<RequisitionKanban>(`/${ENDPOINT}/${id_kanban_requisicao}}`);
-            return response.data;
-        } catch (error: any) {
-            if (error.response && error.response.status === 404) return null;
-            throw error;
-        }
-    },
-
-    async update(id_kanban_requisicao: number, data: Partial<RequisitionKanban>): Promise<RequisitionKanban> {
-        const response = await api.put<RequisitionKanban>(`/${ENDPOINT}/${id_kanban_requisicao}`, data);
-        return response.data;
-    },
-
-    async delete(id_kanban_requisicao: number): Promise<void> {
-        await api.delete(`/${ENDPOINT}/${id_kanban_requisicao}`);
+  async getById(
+    id_kanban_requisicao: number,
+  ): Promise<RequisitionKanban | null> {
+    try {
+      const response = await api.get<RequisitionKanban>(
+        `/${ENDPOINT}/${id_kanban_requisicao}}`,
+      );
+      return response.data;
+    } catch (error: any) {
+      if (error.response && error.response.status === 404) return null;
+      throw error;
     }
+  },
+
+  async update(
+    id_kanban_requisicao: number,
+    data: Partial<RequisitionKanban>,
+  ): Promise<RequisitionKanban> {
+    const response = await api.put<RequisitionKanban>(
+      `/${ENDPOINT}/${id_kanban_requisicao}`,
+      data,
+    );
+    return response.data;
+  },
+
+  async delete(id_kanban_requisicao: number): Promise<void> {
+    await api.delete(`/${ENDPOINT}/${id_kanban_requisicao}`);
+  },
 };
 
 export default RequisitionKanbanService;
